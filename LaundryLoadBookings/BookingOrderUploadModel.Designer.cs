5b8512390f22d6bc3072b6ca2ed82037106ab47f
Darron Absher <d.absher@teammodern.com>
2021-03-03 14:26:17 -0500

    Added support for four digit load numbers.  Added error checking to UploadOrders method.

--- LaundryLoadBookings/BookingOrderUploadModel.Designer.cs
+++ LaundryLoadBookings/BookingOrderUploadModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Source\GitHub\LaundryLoadBookings\LaundryLoadBookings\BookingOrderUploadModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\dabsh\source\repos\LaundryLoadBookings\LaundryLoadBookings\BookingOrderUploadModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
